2d045e494b0b76a44144c814484a37e564ce5541
cosc123 <cosc123Proj@ex.com>
2017-03-26 22:23:34 -0700

    Toast pops up when date is selected, created new journal entry fragment

--- app/src/main/java/ca/amritpal/migrainetracker/ui/entry/JournalEntryFragment.java
+++ app/src/main/java/ca/amritpal/migrainetracker/ui/entry/JournalEntryFragment.java
@@ -1,64 +1,71 @@
-package ca.amritpal.migrainetracker.ui.calendar;
+package ca.amritpal.migrainetracker.ui.entry;
 
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CalendarView;
-import android.widget.Toast;
 
 import ca.amritpal.migrainetracker.R;
-
-import static android.widget.Toast.makeText;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link CalendarFragment.OnFragmentInteractionListener} interface
+ * {@link JournalEntryFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link CalendarFragment# newInstance} factory method to
+ * Use the {@link JournalEntryFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CalendarFragment extends Fragment {
+public class JournalEntryFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private CalendarView mCalendarView;
+    private String mParam1;
+    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    public CalendarFragment() {
+    public JournalEntryFragment() {
         // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment JournalEntryFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static JournalEntryFragment newInstance(String param1, String param2) {
+        JournalEntryFragment fragment = new JournalEntryFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //mCalendarView = (CalendarView) View.findViewById(R.id.calendar_view);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View mCalendarFragmentView = inflater.inflate(R.layout.main_calendar_fragment, container, false);
-
-        mCalendarView = (CalendarView) mCalendarFragmentView.findViewById(R.id.calendar_view);
-
-        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                Toast selectedDateToast = Toast.makeText(getActivity(), "DayOfMonth: "+dayOfMonth, Toast.LENGTH_SHORT);
-                selectedDateToast.show();
-                Log.d("Calendar", "Date Selected");
-            }
-        });
-
         // Inflate the layout for this fragment
-        return mCalendarFragmentView;
+        return inflater.inflate(R.layout.journal_entry_fragment, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
